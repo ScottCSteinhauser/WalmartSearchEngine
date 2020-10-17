342e3af2456261bf17fcf1ca4b0b5151af7734e2
scotts5 <ssteinhauser@tamu.edu>
2020-10-17 16:05:44 -0500

    Added comments and main server file

---
+++ MainServer.py
@@ -0,0 +1,18 @@
+
+# This file will be running, listening for requests from the web server.
+# Requests will include the search text (string), page number (integer), and may include other filters (price range)
+# Upon receiving a request, it will run a search on the products stored, rank the results, and return the top 20 results
+
+
+
+
+
+
+
+
+
+
+
+
+
+
