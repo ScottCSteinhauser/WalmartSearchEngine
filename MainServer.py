714cddade84f96bcecd84dea196c1b7d0388dbc3
scotts5 <ssteinhauser@tamu.edu>
2020-10-17 17:36:04 -0500

    Added comments and main server file

--- MainServer.py
+++ MainServer.py
@@ -1,18 +1,30 @@
+import eventlet
+import socketio
 
-# This file will be running, listening for requests from the web server.
-# Requests will include the search text (string), page number (integer), and may include other filters (price range)
-# Upon receiving a request, it will run a search on the products stored, rank the results, and return the top 20 results
+sio = socketio.Server(cors_allowed_origins='*')
+app = socketio.WSGIApp(sio, static_files={
+    '/': {'content_type': 'text/html', 'filename': 'index.html'}
+})
 
+@sio.event
+def connect(sid, environ):
+    print('connect ', sid)
 
+@sio.event
+def my_message(sid, data):
+    print('message ', data)
 
+@sio.event
+def disconnect(sid):
+    print('disconnect ', sid)
 
+@sio.event
+def execute_search(sid, data):
+    print(sid, data)
+    sio.emit('reply', {'response': data}, room=sid)
 
-
-
-
-
-
-
+if __name__ == '__main__':
+    eventlet.wsgi.server(eventlet.listen(('', 12345)), app)
 
 
 
