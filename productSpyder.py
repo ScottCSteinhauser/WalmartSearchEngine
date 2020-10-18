6e978709b26c406a674df270a8b45754704cb1ee
ljoebaker <ljbaker@tamu.edu>
2020-10-18 08:29:10 -0500

    Scraper w/ multiple starts

--- productSpyder.py
+++ productSpyder.py
@@ -13,14 +13,20 @@ class ProductSpyder(CrawlSpider):
     rules = (
         Rule(LinkExtractor(allow=('/ip/',)), callback="parse_item"),
         Rule(LinkExtractor(allow=('/cp/',))),
-#        Rule(LinkExtractor(allow=('/browse/',))),
+        Rule(LinkExtractor(allow=('/browse/',))),
     )
 
     start_urls = [
-#        'https://www.walmart.com/ip/Rawlings-12-5-RGB36-Recreational-Baseball-Softball-Glove-Right-Hand-Throw/140817752'
-#        'https://www.walmart.com/ip/Super-Smash-Bros-Ultimate-Special-Edition-Nintendo/162583898'
-#        'https://www.walmart.com/ip/Cottonelle-Ultra-CleanCare-Strong-Toilet-Paper-12-Mega-Rolls-Bath-Tissue/331701566?wpa_bd=&wpa_pg_seller_id=F55CDC31AB754BB68FE0B39041159D63&wpa_ref_id=wpaqs:_zkFh0GbX72MldkEenFxk5IufInphxAfGMzSrL_aCMybOMUedwKb2Pd3XgJcLhgAgQSJ6odAn4kKNkJmVifRIMyecEfSqX7E-F74dv7PQG_F1TqFCGD93kzK4vvg3b_gS3KJ99kuOoWiPmv0bPOmqYvHZh49emuOcd_-M9izB9ceCn3cBBgM8HwHbj2xjx9dzvutgEcxFvKSYet36H1sSQ&wpa_tag=__tag__&wpa_aux_info=__aux_info__&wpa_pos=1&wpa_plmt=__plmt__&wpa_aduid=__aduid__'
-#        'https://www.walmart.com/browse/electronics/2-in-1-laptops/3944_3951_1089430_1230091_1155872?page=2'
+        'https://www.walmart.com/cp/clothing/5438?povid=FashionGlobalNav_ShopAll'
+        'https://www.walmart.com/cp/electronics/3944'
+        'https://www.walmart.com/cp/home/4044?povid=4044+%7C+2019-08-30+%7C+ShopAllHomeGFlyout'
+        'https://www.walmart.com/cp/baby-products/5427?povid=BabyGlobalNav_ShopAllBaby'
+        'https://www.walmart.com/cp/toys/4171?&povid=4171+%7C+contentZone20+%7C+2016-11-08+%7C+1+%7C+Toys+Flyout'
+        'https://www.walmart.com/cp/food/976759'
+        'https://www.walmart.com/cp/pet-supplies/5440?povid=5440+%7C+2018-04-30+%7C+all%20pets%20dept%20flyout&povid=5440+%7C+2018-04-30+%7C+super%20dept%20flyout%20all%20pets'
+        'https://www.walmart.com/cp/beauty/1085666?povid=BeautyGlobalNav_ShopAllBeauty'
+        'https://www.walmart.com/cp/personal-care/1005862'
+        'https://www.walmart.com/cp/arts-crafts-and-sewing/1334134'
         'https://walmart.com/all-departments'
     ]
 
